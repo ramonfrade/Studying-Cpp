06b446e1de33b068c1f3c2afd932d1202394d956
Ramon Frade <ramonfrade7@gmail.com>
2024-11-23 21:36:26 -0300

    Updating Chrono.h

--- src/Chapter 9/Exercise/Chrono.h
+++ src/Chapter 9/Exercise/Chrono.h
@@ -62,6 +62,9 @@ namespace Chrono
             return y;
         }
 
+        Date next_workday(const Date& d);
+        int week_of_year(const Date& d);
+
         // modifying operations:
         void add_day(int n);
         void add_month(int n);
@@ -82,8 +85,99 @@ namespace Chrono
     ostream& operator<<(ostream& os, const Date& d);
     istream& operator>>(istream& is, Date& dd);
 
-    Day day_of_week(const Date& d);   // day of week of d
-    Date next_Sunday(const Date d);   // next Sunday after d
+    const Date first_date{1970, Month::jan, 1}; //first day we are using here
+    const Day first_day = Day::thursday;        //first day was a thursday
+
+    // number of leap years between 1970 and Jan 1, year y
+    int n_leapyears(int y)
+    {
+        // go through every year since 1970, check if it is a leap year
+        int nl = 0;
+        for(int i = 1970; i < y; ++i)
+        {
+            if(leapyear(i))
+                ++nl;
+        }
+        return nl;
+    }
+
+    // number of days in the year before Month m, ignoring leap years
+    int nmonth(Month m)
+    {
+        switch(m)
+        {
+            case Month::jan: return 0;
+            case Month::feb: return 31;
+            case Month::mar: return 31 + 28;
+            case Month::apr: return 31 + 28 + 31;
+            case Month::may: return 31 + 28 + 31 + 30;
+            case Month::jun: return 31 + 28 + 31 + 30 + 31;
+            case Month::jul: return 31 + 28 + 31 + 30 + 31 + 30;
+            case Month::aug: return 31 + 28 + 31 + 30 + 31 + 30 + 31;
+            case Month::sep: return 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
+            case Month::oct: return 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
+            case Month::nov: return 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
+            case Month::dec: return 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+        }
+    }
+
+    // number of day in year
+    int day_in_year(const Date& d)
+    {
+        int diy = nmonth(d.month()); // days in months before d
+        diy += d.day();              // days in month of d
+        if(leapyear(d.year()) && d.month() > Month::feb)
+            ++diy; // add day if leap year and later than February
+        return diy;
+    }
+
+    // number of days since Jan 1, 1970 (day 0)
+    long int days_linear(const Date& d)
+    {
+        if(d.year() < first_date.year())
+            error("days_linear: year must be 1970 or later");
+        int y = d.year() - first_date.year();
+        return y * 365 + n_leapyears(d.year()) + day_in_year(d) - 1;
+    }
+
+    Day day_of_week(const Date& d) // day of week of d
+    {
+        if(d.year() < first_date.year())
+            error("day_of_week: works only for 1970 and later");
+        int dlin = days_linear(d);
+        return Day((int(first_day) + dlin) % 7);
+    }
+
+    bool is_workday(const Day& d)
+    {
+        switch(int(d))
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5: return true; break;
+            default: return false; break;
+        }
+    }
+
+    Date Date::next_workday(const Date& d)
+    {
+        Date next_day = d;
+        next_day.add_day(1);
+        day_of_week(next_day);
+        while(!is_workday(day_of_week(next_day))) { next_day.add_day(1); }
+        return next_day;
+    }
+
+    int Date::week_of_year(const Date& d)
+    {
+        Date first_week{d.year(), Month::jan, 1};
+        long int x = days_linear(d) - days_linear(first_week);
+        return 1 + (x / 7);
+    }
+
+    Date next_Sunday(const Date& d);  // next Sunday after d
     Date next_weekday(const Date& d); // next weekday after d
 
     //Month operators functions:
@@ -209,7 +303,11 @@ namespace Chrono
 
     bool leapyear(int y)
     {
-        // see exercise 10
+        if((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+        {
+            return true;
+        }
+        return false;
     }
 
     bool operator==(const Date& a, const Date& b)
